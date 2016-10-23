f2d70e368666b78944adc7d3f7997333a7fbbfd2
Jorge Pla <j.pla88@hotmail.com>
2016-10-23 18:59:12 -0400

    Add files via upload

---
+++ ChangeOfficeAddress/change_address.ps1
@@ -0,0 +1,25 @@
+﻿$credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+
+$user_list = Get-MsolUser | Where City -eq 'Old_City' | select DisplayName, UserPrincipalName
+
+$user_modified = @()
+
+foreach ($user in $user_list) {
+
+    if ($user.City -eq 'Old_City'){
+        
+        Set-MsolUser -UserPrincipalName $user.UserPrincipalName -Office 'New_Office' -StreetAddress 'New_Address' -City 'New_City' -PostalCode 'New_PostalCode' 
+    
+        #Log User modified. 
+        $Object = new-object PSObject
+        $Object | add-member -membertype NoteProperty -name "DisplayName" -Value $user.DisplayName
+        $Object | add-member -membertype NoteProperty -name "UserPrincipalName" -Value $user.UserPrincipalName
+        $user_modified += $Object
+    }
+}
+
+$user_modified | Export-Csv -path C:\User_Modified.csv -NoTypeInformation 
+
+
